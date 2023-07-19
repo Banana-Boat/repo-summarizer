f2acd650b2c4175cab8f6b87b5b352239d1233e2
bananaboat <937499953@qq.com>
2023-07-19 17:00:26 +0800

    feat: 数据集

--- data/generate.py
+++ data/generate.py
@@ -1,20 +1,26 @@
 import json
-import random
+from tqdm import tqdm
 
 with open('./classes_and_interfaces.jsonl', encoding='utf-8') as f:
-    f.readline()
-    lines = f.readlines()
-    random.shuffle(lines)
-    jsonl = json.loads(lines[0])
+    res = []
+    for line in tqdm(f):
+        jsonl = json.loads(line)
 
-    with open('test.java', 'w', encoding='utf-8') as f:
-        f.write('/**\n')
-        f.write(' * ' + jsonl['des'] + '\n')
-        f.write(' * ' + jsonl['repo'] + '\n')
-        f.write(' */\n')
-        f.write(jsonl['signature'] + ' {\n')
+        obj = {}
+
+        code = jsonl['signature'] + ' {\n'
         for method in jsonl['methods_des']:
             if method['method_des'] != '':
-                f.write('\t// ' + method['method_des'] + '\n')
-            f.write('\t' + method['method_name'] + ';\n')
-        f.write('}')
+                code += '\t// ' + method['method_des'] + '\n'
+            code += '\t' + method['method_name'] + ';\n'
+        code += '}'
+
+        obj['index'] = jsonl['index']
+        obj['code'] = code
+        obj['des'] = jsonl['des']
+
+        res.append(obj)
+
+    with open('valid.jsonl', 'w', encoding='utf-8') as f:
+        for item in res:
+            f.write(json.dumps(item, ensure_ascii=False) + '\n')
