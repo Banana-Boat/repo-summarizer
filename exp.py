b6721e77ed2f35ddcc823e519dcd9443966ddfd6
bananaboat <937499953@qq.com>
2023-07-20 10:40:53 +0800

    update

--- exp.py
+++ exp.py
@@ -1,4 +1,6 @@
 import datetime
+import random
+from time import sleep
 from bleu import computeMaps, bleuFromMaps
 from tqdm import tqdm
 from transformers import RobertaTokenizer, T5ForConditionalGeneration
@@ -8,18 +10,19 @@ if __name__ == '__main__':
     model_name = 'codet5-base-multi-sum'
 
     tokenizer = RobertaTokenizer.from_pretrained('Salesforce/' + model_name)
-    model = T5ForConditionalGeneration.from_pretrained('Salesforce/' + model_name)
+    model = T5ForConditionalGeneration.from_pretrained(
+        'Salesforce/' + model_name)
 
     predictions = []
 
     with open('./data/valid.jsonl', "r") as valid_file, open('./log/valid.out', "w", encoding="utf-8") as out_file, open('./log/valid.gold', "w", encoding="utf-8") as gold_file:
-        for line in tqdm(valid_file):
+        lines = valid_file.readlines()
+        random.shuffle(lines)
+        for line in tqdm(lines[:2000]):
             obj = json.loads(line)
 
-            if obj['index'] == 1000:
-                break
-
-            input_ids = tokenizer(obj['code'], max_length=510, truncation=True, return_tensors="pt").input_ids
+            input_ids = tokenizer(
+                obj['code'], max_length=510, truncation=True, return_tensors="pt").input_ids
             generated_ids = model.generate(input_ids, max_length=20)
             res = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
 
@@ -27,15 +30,12 @@ if __name__ == '__main__':
             out_file.write(str(obj['index']) + '\t' + res + '\n')
             gold_file.write(str(obj['index']) + '\t' + obj['des'] + '\n')
 
-
     (goldMap, predictionMap) = computeMaps(predictions, "./log/valid.gold")
     this_bleu = round(bleuFromMaps(goldMap, predictionMap)[0], 2)
 
     print(this_bleu)
 
-
     # log_filename = '{}_{}_{}.txt'.format(class_name, model_name, datetime.datetime.now().strftime("%m%d_%H%M"))
     # with open('./log/' + log_filename, mode="w", encoding='utf-8') as f:
     #     f.write('result: {}\n'.format(res))
     #     f.write('ground truth: {}\n'.format(class_des))
-
