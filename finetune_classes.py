4e78b98ed5b609bb25dc0c2a50f5c7d589a47672
Banana-Boat <937499953@qq.com>
2023-07-26 19:35:36 +0800

    feat: 微调class

--- finetune_classes.py
+++ finetune_classes.py
@@ -73,9 +73,9 @@ def read_arguments():
     parser.add_argument("--num_train_epochs", default=24, type=int,
                         help="Total number of training epochs to perform.")
 
-    parser.add_argument("--train_batch_size", default=52, type=int,
+    parser.add_argument("--train_batch_size", default=40, type=int,
                         help="Batch size per GPU/CPU for training.")
-    parser.add_argument("--eval_batch_size", default=26, type=int,
+    parser.add_argument("--eval_batch_size", default=20, type=int,
                         help="Batch size per GPU/CPU for evaluation.")
     parser.add_argument('--gradient_accumulation_steps', type=int, default=2,
                         help="Number of updates steps to accumulate before performing a backward/update pass.")
